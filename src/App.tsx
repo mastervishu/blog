bf1d57127cce5d338cb763e4e6f1dc10b7cc1996
vishuDev <vishalrawat979@gmail.com>
2024-01-27 11:46:07 +0530

    fix bug on bg-changer functionality and add duration 500ms while user change the theme.

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <ThemeProvider value={{ theme, lightTheme, darkTheme }}>
       <Navbar />
-      <div className="mt-[69px] w-full h-[90.1vh] bg-white dark:bg-gray-900 duration-200  flex items-center justify-center">
+      <div className="mt-[69px] w-full h-[90.1vh] bg-white dark:bg-gray-900 flex items-center justify-center duration-500">
         <button type="button" onClick={handleChange} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Theme Changer</button>
 
       </div>
